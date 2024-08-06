fdc0c2e7f67f748f4dbfa41cdea2e9602fc0df54
hophuoc4so9 <2224802010872@student.tdmu.edu.vn>
2024-08-06 08:43:39 +0700

    bai tap 7.2

--- src/btvn/7-2/Todolist.tsx
+++ src/btvn/7-2/Todolist.tsx
@@ -2,25 +2,29 @@ import { useState } from "react";
 
 
  function toDoList() {
-    // const [jobs, setJobs] = useState<string[]>( () => 
-    // {
-    //     const jobStorage = localStorage.getItem('jobs') ? JSON.parse(localStorage.getItem('jobs')!) : [];
-    //     return jobStorage ?? [];
-    // }
-    // );
-    const [jobs, setJobs] = useState<string[]>()
+    const [jobs, setJobs] = useState<string[]>( () => 
+    {
+        const  jobStorage = localStorage.getItem('jobs') ? JSON.parse(localStorage.getItem('jobs')!) : [];
+     
+        return jobStorage || [];
+    }
+    );
+  // const [jobs, setJobs] = useState<string[]>()
     const [job, setJob] = useState("");
     const handleSumit = () =>
         {
+         
             setJobs(pre=>{
-               const newjobs= [...pre,job]
-               const jsonjob=JSON.stringify(job);
+                const newjobs = [...(pre || []), job];
+               const jsonjob=JSON.stringify(newjobs);
                localStorage.setItem('jobs',jsonjob);
                return newjobs
             })
         } 
     return(
+     
         <div>
+         
             <div>
                 <input
                  value={job}
@@ -31,9 +35,11 @@ import { useState } from "react";
             <div>
                 <ul>
                     {
-                        jobs.map((job,index)=>
-                            <li key={index}>{job}</li>
-                        )
+                    
+                     jobs?.map((job,index)=>
+                                <li key={index}>{job}</li>
+                            )
+                        
                     }
                 </ul>
             </div>
